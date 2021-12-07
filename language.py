1a38be53744ea11b5281fefb1f699a661868f222
Robert Bogan Kang <bob.kang@qq.com>
2021-12-07 13:01:41 +0800

    .

--- language.py
+++ language.py
@@ -4,11 +4,11 @@ from midi_language import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='encode & decode midi test with MIDI Language')
-    parser.add_argument('--input', '-i', type=str, help='input file of audio file to encode / decode',
+    parser.add_argument('--input', '-i', type=str, help='input file of audio file to encode/decode',
                         default='demo/in.mid')
     parser.add_argument('--output', '-o', type=str, help='output file of audio file to encode',
                         default='demo/out.mid')
-    parser.add_argument('--train', '-t', action='store_true', help='training mode with variations')
+    parser.add_argument('--train', '-t', action='store_true', help='training mode with variations (data augmentation)')
     args = parser.parse_args()
 
     # initialize dict once
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     if args.train:
         me = MidiEncoder(args.input, w2i,
                          pitch_variation_range=(-12, 12),
-                         velocity_scale_variation_range=(0.6, 1.2),
+                         velocity_scale_variation_range=(0.8, 1),
                          velocity_noise_scale_variation_range=(0.95, 1.05),
                          tempo_scale_variation_range=(0.7, 1.4))
     else:
